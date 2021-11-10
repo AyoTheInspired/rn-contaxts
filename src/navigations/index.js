dfac3cda9f8fa5895a1b1e3f2973897cc88440d0
AyoTheInspired <ayoinspired@gmail.com>
2021-11-10 21:20:06 +0100

    Screens have been configured as components

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./AuthNavigator";
-import HomeNavigator from "./HomeNavigator";
 import DrawerNavigator from "./DrawerNavigator";
 
 const AppNavContainer = () => {
